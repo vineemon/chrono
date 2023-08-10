1b49e6403987880487c2e7a9898446b5038d71ce
vineemon <nadella.vineet@gatech.edu>
2023-08-10 18:38:55 -0400

    Initial Commit

---
+++ ChronoService/ChronoServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChronoServiceApp.swift
+//  ChronoService
+//
+//  Created by Vineet Nadella on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ChronoServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
